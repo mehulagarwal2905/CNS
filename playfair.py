2873ea3528d34b6de85405b82e1cdf80de55d5e5
Mehul <mehulagarwalsg2905@gmail.com>
2025-07-30 15:44:24 +0530

    Playfair

---
+++ playfair.py
@@ -0,0 +1,93 @@
+def create_matrix(key):
+    key = key.upper().replace("J", "I")
+    matrix = []
+    seen = set()
+
+    for char in key:
+        if char not in seen and char.isalpha():
+            matrix.append(char)
+            seen.add(char)
+
+    for char in "ABCDEFGHIKLMNOPQRSTUVWXYZ":
+        if char not in seen:
+            matrix.append(char)
+
+    return [matrix[i*5:(i+1)*5] for i in range(5)]
+
+def find_position(matrix, letter):
+    for row in range(5):
+        for col in range(5):
+            if matrix[row][col] == letter:
+                return row, col
+    return None, None
+
+def preprocess_text(text):
+    text = text.upper().replace("J", "I").replace(" ", "")
+    processed = ""
+    i = 0
+    while i < len(text):
+        a = text[i]
+        b = text[i+1] if i+1 < len(text) else "X"
+        if a == b:
+            processed += a + "X"
+            i += 1
+        else:
+            processed += a + b
+            i += 2
+    if len(processed) % 2 != 0:
+        processed += "X"
+    return processed
+
+def encrypt_playfair(plain_text, key):
+    matrix = create_matrix(key)
+    text = preprocess_text(plain_text)
+    cipher = ""
+
+    for i in range(0, len(text), 2):
+        a, b = text[i], text[i+1]
+        r1, c1 = find_position(matrix, a)
+        r2, c2 = find_position(matrix, b)
+
+        if r1 == r2:
+            cipher += matrix[r1][(c1 + 1) % 5]
+            cipher += matrix[r2][(c2 + 1) % 5]
+        elif c1 == c2:
+            cipher += matrix[(r1 + 1) % 5][c1]
+            cipher += matrix[(r2 + 1) % 5][c2]
+        else:
+            cipher += matrix[r1][c2]
+            cipher += matrix[r2][c1]
+
+    return cipher
+
+def decrypt_playfair(cipher_text, key):
+    matrix = create_matrix(key)
+    plain = ""
+
+    for i in range(0, len(cipher_text), 2):
+        a, b = cipher_text[i], cipher_text[i+1]
+        r1, c1 = find_position(matrix, a)
+        r2, c2 = find_position(matrix, b)
+
+        if r1 == r2:
+            plain += matrix[r1][(c1 - 1) % 5]
+            plain += matrix[r2][(c2 - 1) % 5]
+        elif c1 == c2:
+            plain += matrix[(r1 - 1) % 5][c1]
+            plain += matrix[(r2 - 1) % 5][c2]
+        else:
+            plain += matrix[r1][c2]
+            plain += matrix[r2][c1]
+
+    return plain
+
+# Example usage
+key = input("Enter the key for Playfair cipher: ")
+
+plaintext = input("Enter the plaintext to encrypt: ")
+ciphertext = encrypt_playfair(plaintext, key)
+decrypted = decrypt_playfair(ciphertext, key)
+
+print("Plaintext:", plaintext)
+print("Encrypted:", ciphertext)
+print("Decrypted:", decrypted)
